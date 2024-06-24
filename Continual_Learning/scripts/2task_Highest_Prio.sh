9d5e5ebef241ca2b1cd463094bd59710b0c457b2
EthanVeselka <ethan.veselka@gmail.com>
2024-06-24 03:29:20 -0500

    Adjusted script for Phen/LoS tests

--- Continual_Learning/scripts/2task_Highest_Prio.sh
+++ Continual_Learning/scripts/2task_Highest_Prio.sh
@@ -68,25 +68,25 @@ python3 ../tests/test.py --region 4 --tasks 2 --phen --b 500 --replay --test --r
 
 # Decompensation: (1 epoch, 100k:100k:100k:50k:25k samples)
 # --------------------------------#
-python3 ../tests/test.py --region 1 --tasks 2 --dec --test --rt --i 5                                  # Baseline
-python3 ../tests/test.py --region 2 --tasks 2 --dec --test --rt --i 5                                  # Baseline
-python3 ../tests/test.py --region 3 --tasks 2 --dec --test --rt --i 5                                  # Baseline
-python3 ../tests/test.py --region 4 --tasks 2 --dec --test --rt --i 5                                  # Baseline
+# python3 ../tests/test.py --region 1 --tasks 2 --dec --test --rt --i 5                                  # Baseline
+# python3 ../tests/test.py --region 2 --tasks 2 --dec --test --rt --i 5                                  # Baseline
+# python3 ../tests/test.py --region 3 --tasks 2 --dec --test --rt --i 5                                  # Baseline
+# python3 ../tests/test.py --region 4 --tasks 2 --dec --test --rt --i 5                                  # Baseline
 
-python3 ../tests/test.py --region 1 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
-python3 ../tests/test.py --region 2 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
-python3 ../tests/test.py --region 3 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
-python3 ../tests/test.py --region 4 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
+# python3 ../tests/test.py --region 1 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
+# python3 ../tests/test.py --region 2 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
+# python3 ../tests/test.py --region 3 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
+# python3 ../tests/test.py --region 4 --tasks 2 --dec --b 3500 --replay --ewc --imp 6 --test --rt --i 5  # Combined
 
-python3 ../tests/test.py --region 1 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
-python3 ../tests/test.py --region 2 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
-python3 ../tests/test.py --region 3 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
-python3 ../tests/test.py --region 4 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
+# python3 ../tests/test.py --region 1 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
+# python3 ../tests/test.py --region 2 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
+# python3 ../tests/test.py --region 3 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
+# python3 ../tests/test.py --region 4 --tasks 2 --dec --b 3500 --replay --test --rt --i 5                # Replay
 
-python3 ../tests/test.py --region 1 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
-python3 ../tests/test.py --region 2 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
-python3 ../tests/test.py --region 3 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
-python3 ../tests/test.py --region 4 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
+# python3 ../tests/test.py --region 1 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
+# python3 ../tests/test.py --region 2 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
+# python3 ../tests/test.py --region 3 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
+# python3 ../tests/test.py --region 4 --tasks 2 --dec --b 3500 --ewc --imp 6 --test --rt --i 5           # EWC
 
 
 # LoS has not been thoroughly tested, parameters are educated guesses, 
